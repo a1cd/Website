93a148de4a9566ff478afd4229c802e862ac2e2d
a1cd <71281043+a1cd@users.noreply.github.com>
2021-05-16 12:44:01 -0400

    Created Veracross site

---
+++ Sites/My Other app/resizeIframes.js
@@ -0,0 +1,13 @@
+function resizeIFrameToFitContent( iFrame ) {
+    iFrame.width  = iFrame.contentWindow.document.body.scrollWidth;
+    iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
+}
+window.addEventListener('DOMContentLoaded', function(e) {
+    // or, to resize all iframes:
+    var iframes = top.frames;
+    for( var i = 0; i < iframes.length; i++) {
+        // iframes[i].contentWindow.document.addEventListener("")
+        let curFram = iframes[i]
+        curFram.addEventListener("load", resizeIFrameToFitContent( iframes[i] ));
+    }
+} );
